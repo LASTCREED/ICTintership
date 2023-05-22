842e3b8c27959e779685481a90122e7ae9d15fc9
Albin Bejoy <134275843+LASTCREED@users.noreply.github.com>
2023-05-22 16:00:07 +0530

    Add files via upload

---
+++ activity1.js
@@ -0,0 +1,16 @@
+let time=12;
+if(time<=11)
+{
+    console.log("Good morning");
+}
+else if(time>=13 && time<=15)
+{
+    console.log("Good afternoon");
+}
+else if(time>=16 && time<=18)
+{
+    console.log("Good evening");
+}
+else{
+    console.log("Good night");
+}
